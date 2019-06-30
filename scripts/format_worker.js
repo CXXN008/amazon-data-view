c1acf498f91aed06c73befaa33a2f6bc470cd6f8
cccccccccccccccccccccccccccccccccccccc <linus3389@gmail.com>
2019-07-01 03:04:57 +0800

    css adjust

--- scripts/format_worker.js
+++ scripts/format_worker.js
@@ -5,20 +5,20 @@ onmessage = e => {
         const element = items[i];
         let checkTemplate = ''
         let itemElem = '';
-        ['best', 'achoice', 'aplus', 'video', 'image', 'pvideo'].forEach(e => {
-            let hStr = `
-              <div class="pretty p-svg p-curve p-locked p-check">
-                    <input class="card-`+ e + `" type="checkbox" ` + (element[e] > 0 ? 'checked' : '') + ` />
-                    <div class="state p-success">
-                        <svg class="svg svg-icon" viewBox="0 0 20 20">
-                            <path d="M7.629,14.566c0.125,0.125,0.291,0.188,0.456,0.188c0.164,0,0.329-0.062,0.456-0.188l8.219-8.221c0.252-0.252,0.252-0.659,0-0.911c-0.252-0.252-0.659-0.252-0.911,0l-7.764,7.763L4.152,9.267c-0.252-0.251-0.66-0.251-0.911,0c-0.252,0.252-0.252,0.66,0,0.911L7.629,14.566z" style="stroke: white;fill:white;"></path>
-                        </svg>
-                        <label>`+ e + `</label>
-                    </div>
-                </div>
-              `
-            checkTemplate += hStr
-        });
+        // ['best', 'achoice', 'aplus', 'video', 'image', 'pvideo'].forEach(e => {
+        //     let hStr = `
+        //       <div class="pretty p-svg p-curve p-locked p-check">
+        //             <input class="card-`+ e + `" type="checkbox" ` + (element[e] > 0 ? 'checked' : '') + ` />
+        //             <div class="state p-success">
+        //                 <svg class="svg svg-icon" viewBox="0 0 20 20">
+        //                     <path d="M7.629,14.566c0.125,0.125,0.291,0.188,0.456,0.188c0.164,0,0.329-0.062,0.456-0.188l8.219-8.221c0.252-0.252,0.252-0.659,0-0.911c-0.252-0.252-0.659-0.252-0.911,0l-7.764,7.763L4.152,9.267c-0.252-0.251-0.66-0.251-0.911,0c-0.252,0.252-0.252,0.66,0,0.911L7.629,14.566z" style="stroke: white;fill:white;"></path>
+        //                 </svg>
+        //                 <label>`+ e + `</label>
+        //             </div>
+        //         </div>
+        //       `
+        //     checkTemplate += hStr
+        // });
 
 
         let itemTemplate = '' +
@@ -40,13 +40,13 @@ onmessage = e => {
             '</div>' +
             '<div class="card-price">$' + element.price + '</div>' +
             '</div>' +
-            '<div class="card-check">' +
-            checkTemplate +
-            '</div>' +
+            // '<div class="card-check">' +
+            // checkTemplate +
+            // '</div>' +
             '<div class="card-extra">' +
             '<div class="card-bot card-date">上市: ' + getLocalTime(element.date_first_on).split(' ')[0] + '</div>' +
-            '<div class="card-bot card-comment">评价: ' + element.ask + '</div>' +
-            '<div class="card-bot card-ask">问答: ' + element.reviews + '</div>' +
+            '<div class="card-bot card-comment">评价: ' + element.reviews + '</div>' +
+            '<div class="card-bot card-ask">问答: ' + element.ask + '</div>' +
             '</div>' +
             '<div class="card-remove"><i class="material-icons">&#xE5CD;</i></div>' +
             '</div>' +
--- scripts/main.js
+++ scripts/main.js
@@ -37,15 +37,32 @@ document.addEventListener('DOMContentLoaded', function () {
   let maxAsksField = demo.querySelector('.max-asks-field');
 
   let sDateField = demo.querySelector('.s-date-field');
-  let topField = demo.querySelector('.top-field');
+  let topField = demo.querySelector
+  ('.top-field');
 
 
-  let bestField = demo.querySelector('.best-field');
-  let achoiceField = demo.querySelector('.achoice-field');
-  let aplusField = demo.querySelector('.aplus-field');
-  let videoField = demo.querySelector('.video-field');
-  let imageField = demo.querySelector('.image-field');
-  let pvideoField = demo.querySelector('.pvideo-field');
+  let btnClear = document.getElementById('btn-clear');
+  let btnSubmit = document.getElementById('btn-submit');
+  let btnAnalyze = document.getElementById('btn-analyze');
+
+
+  btnAnalyze.addEventListener('click',()=>{
+    let asinList =[]
+    grid.getItems().forEach(e => {
+      asinList.push(e.getElement().querySelector('.card-asin').innerText)
+    })
+    window.localStorage.setItem('asinList',asinList)
+    console.log(window.localStorage.getItem('asinList'));
+    window.open('analysis.html','_blank')
+  })
+
+
+  // let bestField = demo.querySelector('.best-field');
+  // let achoiceField = demo.querySelector('.achoice-field');
+  // let aplusField = demo.querySelector('.aplus-field');
+  // let videoField = demo.querySelector('.video-field');
+  // let imageField = demo.querySelector('.image-field');
+  // let pvideoField = demo.querySelector('.pvideo-field');
 
 
   // let sortField = demo.querySelector('.sort-field');
@@ -71,9 +88,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Reset field values.
     searchField.value = 'B01C89GCHU';
-    [bestField, achoiceField, aplusField, videoField, imageField, pvideoField].forEach(function (field) {
-      field.value = field.querySelectorAll('option')[0].value;
-    });
+    // [bestField, achoiceField, aplusField, videoField, imageField, pvideoField].forEach(function (field) {
+    //   field.value = field.querySelectorAll('option')[0].value;
+    // });
 
     // Set inital search query, active filter, active sort value and active layout.
     let vAnd = andField.value.toLowerCase()
@@ -180,12 +197,12 @@ document.addEventListener('DOMContentLoaded', function () {
     //top先不管
     sDateField.addEventListener('change', filter);
 
-    bestField.addEventListener('change', filter);
-    achoiceField.addEventListener('change', filter);
-    aplusField.addEventListener('change', filter);
-    videoField.addEventListener('change', filter);
-    imageField.addEventListener('change', filter);
-    pvideoField.addEventListener('change', filter);
+    // bestField.addEventListener('change', filter);
+    // achoiceField.addEventListener('change', filter);
+    // aplusField.addEventListener('change', filter);
+    // videoField.addEventListener('change', filter);
+    // imageField.addEventListener('change', filter);
+    // pvideoField.addEventListener('change', filter);
 
 
 
@@ -258,24 +275,24 @@ document.addEventListener('DOMContentLoaded', function () {
       .on('move', updateIndices)
       .on('sort', updateIndices);
 
-    let data = {
-      "asin": searchField.value,
-      "and_keyword": andField.value.split(' '),
-      "or_keyword": orField.value.split(' '),
-      "not_keyword": notField.value.split(' '),
-      "star": { "s": minRateField.value, "e": maxRateField.value },
-      "price": { "s": minPriceField.value, "e": maxPriceField.value },
-      "reviews": { "s": minCommentsField.value, "e": maxCommentsField.value },
-      "ask": { "s": minAsksField.value, "e": maxAsksField.value },
-      "date": { "s": isNaN(getMillTime(sPicker.selectedDates[0])) ? '' : getMillTime(sPicker.selectedDates[0]), "e": isNaN(getMillTime(sPicker.selectedDates[0])) ? '' : getMillTime(sPicker.selectedDates[1]) },
-      "best": bestField.selectedOptions[0].value,
-      "achoice": achoiceField.selectedOptions[0].value,
-      "image": imageField.selectedOptions[0].value,
-      "aplus": aplusField.selectedOptions[0].value,
-      "video": videoField.selectedOptions[0].value,
-      "pvideo": pvideoField.selectedOptions[0].value,
-      "top": topField.value
-    }
+    // let data = {
+    //   "asin": searchField.value,
+    //   "and_keyword": andField.value.split(' '),
+    //   "or_keyword": orField.value.split(' '),
+    //   "not_keyword": notField.value.split(' '),
+    //   "star": { "s": minRateField.value, "e": maxRateField.value },
+    //   "price": { "s": minPriceField.value, "e": maxPriceField.value },
+    //   "reviews": { "s": minCommentsField.value, "e": maxCommentsField.value },
+    //   "ask": { "s": minAsksField.value, "e": maxAsksField.value },
+    //   "date": { "s": isNaN(getMillTime(sPicker.selectedDates[0])) ? '' : getMillTime(sPicker.selectedDates[0]), "e": isNaN(getMillTime(sPicker.selectedDates[0])) ? '' : getMillTime(sPicker.selectedDates[1]) },
+    //   "best": bestField.selectedOptions[0].value,
+    //   "achoice": achoiceField.selectedOptions[0].value,
+    //   "image": imageField.selectedOptions[0].value,
+    //   "aplus": aplusField.selectedOptions[0].value,
+    //   "video": videoField.selectedOptions[0].value,
+    //   "pvideo": pvideoField.selectedOptions[0].value,
+    //   "top": topField.value
+    // }
 
     // https://images-na.ssl-images-amazon.com/images
     let url =
@@ -300,7 +317,9 @@ document.addEventListener('DOMContentLoaded', function () {
               item.innerHTML = element
               items.push(item.firstChild)
             });
-            grid.add(items)
+            grid.add(items
+              // .slice(0,30)
+              )
           }
         } else {
           console.log("Browser doesn't support web workers.")
@@ -329,12 +348,12 @@ document.addEventListener('DOMContentLoaded', function () {
     let vEDate = getMillTime(sPicker.selectedDates[1])
     let vTop = topField.value
 
-    let vBest = bestField.selectedOptions[0].value
-    let vAchoice = achoiceField.selectedOptions[0].value
-    let vAplus = aplusField.selectedOptions[0].value
-    let vVideo = videoField.selectedOptions[0].value
-    let vImage = imageField.selectedOptions[0].value
-    let vPvideo = pvideoField.selectedOptions[0].value
+    // let vBest = bestField.selectedOptions[0].value
+    // let vAchoice = achoiceField.selectedOptions[0].value
+    // let vAplus = aplusField.selectedOptions[0].value
+    // let vVideo = videoField.selectedOptions[0].value
+    // let vImage = imageField.selectedOptions[0].value
+    // let vPvideo = pvideoField.selectedOptions[0].value
 
 
     grid.filter(function (item) {
@@ -345,12 +364,12 @@ document.addEventListener('DOMContentLoaded', function () {
       let comment = element.querySelector('.card-comment').innerText.replace('评价: ', '')
       let ask = element.querySelector('.card-ask').innerText.replace('问答: ', '')
       let price = element.querySelector('.card-price').innerText.replace('$', '')
-      let best = element.querySelector('.card-best').checked ? 1 : 0
-      let achoice = element.querySelector('.card-achoice').checked ? 1 : 0
-      let aplus = element.querySelector('.card-aplus').checked ? 1 : 0
-      let video = element.querySelector('.card-video').checked ? 1 : 0
-      let image = element.querySelector('.card-image').checked ? 1 : 0
-      let pvideo = element.querySelector('.card-pvideo').checked ? 1 : 0
+      // let best = element.querySelector('.card-best').checked ? 1 : 0
+      // let achoice = element.querySelector('.card-achoice').checked ? 1 : 0
+      // let aplus = element.querySelector('.card-aplus').checked ? 1 : 0
+      // let video = element.querySelector('.card-video').checked ? 1 : 0
+      // let image = element.querySelector('.card-image').checked ? 1 : 0
+      // let pvideo = element.querySelector('.card-pvideo').checked ? 1 : 0
 
 
 
@@ -362,35 +381,27 @@ document.addEventListener('DOMContentLoaded', function () {
       let isCommentMatch = !vMinComment && !vMaxComment ? true : comment > vMinComment && comment < vMaxComment
       let isPriceMatch = !vMinPrice && !vMaxPrice ? true : price > vMinPrice && price < vMaxPrice
       let isAskMatch = !vMinAsks && !vMaxAsks ? true : ask > vMinAsks && ask < vMaxAsks
-      let isBestMatch = !vBest || vBest == -1 ? true : best == vBest
-      let isAchoiceMatch = !vAchoice || vAchoice == -1 ? true : achoice == vAchoice
-      let isAplusMatch = !vAplus || vAplus == -1 ? true : aplus == vAplus
-      let isVdieoMatch = !vVideo || vVideo == -1 ? true : video == vVideo
-      let isImageMatch = !vImage || vImage == -1 ? true : image == vImage
-      let isPvideoMatch = !vPvideo || vPvideo == -1 ? true : pvideo == vPvideo
+      // let isBestMatch = !vBest || vBest == -1 ? true : best == vBest
+      // let isAchoiceMatch = !vAchoice || vAchoice == -1 ? true : achoice == vAchoice
+      // let isAplusMatch = !vAplus || vAplus == -1 ? true : aplus == vAplus
+      // let isVdieoMatch = !vVideo || vVideo == -1 ? true : video == vVideo
+      // let isImageMatch = !vImage || vImage == -1 ? true : image == vImage
+      // let isPvideoMatch = !vPvideo || vPvideo == -1 ? true : pvideo == vPvideo
 
-      console.log(
-        'isBestMatch: ', isBestMatch,
-        'isAchoiceMatch: ', isAchoiceMatch,
-        'isAplusMatch: ', isAplusMatch,
-        'isVdieoMatch: ', isVdieoMatch,
-        'isImageMatch: ', isImageMatch,
-        'isPvideoMatch: ', isPvideoMatch,
-
-      );
 
       return ((isAndMatch || isOrMatch) && !isNotMatch) &&
         isDateMatch &&
         isRateMatch &&
         isCommentMatch &&
         isPriceMatch &&
-        isAskMatch &&
-        isBestMatch &&
-        isAchoiceMatch &&
-        isAplusMatch &&
-        isVdieoMatch &&
-        isImageMatch &&
-        isPvideoMatch
+        isAskMatch 
+        // &&
+        // isBestMatch &&
+        // isAchoiceMatch &&
+        // isAplusMatch &&
+        // isVdieoMatch &&
+        // isImageMatch &&
+        // isPvideoMatch
 
       // && isFilterMatch;
     });
@@ -502,20 +513,20 @@ document.addEventListener('DOMContentLoaded', function () {
       const element = items[i];
       let checkTemplate = ''
       let itemElem = document.createElement('div');
-      ['best', 'achoice', 'aplus', 'video', 'image', 'pvideo'].forEach(e => {
-        let hStr = `
-          <div class="pretty p-svg p-curve p-locked p-check">
-                <input class="card-`+ e + `" type="checkbox" ` + (element[e] > 0 ? 'checked' : '') + ` />
-                <div class="state p-success">
-                    <svg class="svg svg-icon" viewBox="0 0 20 20">
-                        <path d="M7.629,14.566c0.125,0.125,0.291,0.188,0.456,0.188c0.164,0,0.329-0.062,0.456-0.188l8.219-8.221c0.252-0.252,0.252-0.659,0-0.911c-0.252-0.252-0.659-0.252-0.911,0l-7.764,7.763L4.152,9.267c-0.252-0.251-0.66-0.251-0.911,0c-0.252,0.252-0.252,0.66,0,0.911L7.629,14.566z" style="stroke: white;fill:white;"></path>
-                    </svg>
-                    <label>`+ e + `</label>
-                </div>
-            </div>
-          `
-        checkTemplate += hStr
-      });
+      // ['best', 'achoice', 'aplus', 'video', 'image', 'pvideo'].forEach(e => {
+      //   let hStr = `
+      //     <div class="pretty p-svg p-curve p-locked p-check">
+      //           <input class="card-`+ e + `" type="checkbox" ` + (element[e] > 0 ? 'checked' : '') + ` />
+      //           <div class="state p-success">
+      //               <svg class="svg svg-icon" viewBox="0 0 20 20">
+      //                   <path d="M7.629,14.566c0.125,0.125,0.291,0.188,0.456,0.188c0.164,0,0.329-0.062,0.456-0.188l8.219-8.221c0.252-0.252,0.252-0.659,0-0.911c-0.252-0.252-0.659-0.252-0.911,0l-7.764,7.763L4.152,9.267c-0.252-0.251-0.66-0.251-0.911,0c-0.252,0.252-0.252,0.66,0,0.911L7.629,14.566z" style="stroke: white;fill:white;"></path>
+      //               </svg>
+      //               <label>`+ e + `</label>
+      //           </div>
+      //       </div>
+      //     `
+      //   checkTemplate += hStr
+      // });
 
 
       let itemTemplate = '' +
@@ -537,13 +548,13 @@ document.addEventListener('DOMContentLoaded', function () {
         '</div>' +
         '<div class="card-price">$' + element.price + '</div>' +
         '</div>' +
-        '<div class="card-check">' +
-        checkTemplate +
-        '</div>' +
+        // '<div class="card-check">' +
+        // checkTemplate +
+        // '</div>' +
         '<div class="card-extra">' +
         '<div class="card-bot card-date">上市: ' + getLocalTime(element.date_first_on).split(' ')[0] + '</div>' +
-        '<div class="card-bot card-comment">评价: ' + element.ask + '</div>' +
-        '<div class="card-bot card-ask">问答: ' + element.reviews + '</div>' +
+        '<div class="card-bot card-comment">评价: ' + element.reviews + '</div>' +
+        '<div class="card-bot card-ask">问答: ' + element.ask + '</div>' +
         '</div>' +
         '<div class="card-remove"><i class="material-icons">&#xE5CD;</i></div>' +
         '</div>' +
